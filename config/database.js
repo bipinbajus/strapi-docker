c7fa57b56ee7c47733f0cd1151a30701362dfbc3
BipinBaj <bipinjz@gmail.com>
2022-01-31 16:20:40 +1100

    update database to mysql aws

--- config/database.js
+++ config/database.js
@@ -1,10 +1,18 @@
 const path = require('path');
+require('dotenv').config()
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'mysql',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', process.env.DATABASE_HOST ),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', process.env.DATABASE_NAME),
+      user: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
+      password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
     useNullAsDefault: true,
   },
