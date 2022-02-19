f938fea4db9646735e572475af0ebfa5a831be9b
BipinBaj <bipinjz@gmail.com>
2022-02-19 14:43:06 +1100

    upgrade 4.0.8 and install forms plugin

--- config/server.js
+++ config/server.js
@@ -1,4 +1,10 @@
+require('dotenv').config()
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  app: {
+    keys: env.array("APP_KEYS", process.env.APP_KEYS),
+  },
+  // ...
 });
